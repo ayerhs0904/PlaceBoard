831be6ea5997f71b73b22f2b28dbc18b7b4736a9
ayerhs0904 <shreya2212s@gmail.com>
2026-06-26 14:39:13 +0530

    chore: spring boot placeboard project setup

---
+++ src/main/java/com/placeboard/PlaceboardApplication.java
@@ -0,0 +1,13 @@
+package com.placeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceboardApplication.class, args);
+    }
+
+}
